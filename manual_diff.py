b3b26d8ca72692e296642323b0de90b53c755662
OGSILVER <dragoszemlinschi@gmail.com>
2026-02-10 17:52:13 +0200

    added test scrip to compare with new lib

--- manual_diff.py
+++ manual_diff.py
@@ -1,17 +1,19 @@
 from numpy import asarray
 from PIL import Image
+from SSIM_PIL import compare_ssim as ssim
 
 from crop_icons import ICON_SIZE
 
 
+image1 = Image.open("BOOSTS/FOCUS.png")
+image2 = Image.open("focus1.png")
+image2 = image2.convert("L")                                
 
-for i in range(1,19):
-    diff = 0
-    icon1 = asarray(Image.open("BOOSTS/FOCUS.png"))
-    icon2 = asarray(Image.open(f"block{i}.png"))
-    for i in range(ICON_SIZE):
-        for j in range(ICON_SIZE):
-            diff += abs(int(icon1[i][j]) - int(icon2[i][j]))
+image1 = image1.crop((0, 0, ICON_SIZE, ICON_SIZE//2+4))
+image2 = image2.crop((0, 0, ICON_SIZE, ICON_SIZE//2+4))
+image1.save("img1_crop.png")
+image2.save("img2_crop.png")
 
+value = ssim(image2, image1)
 
-    print(str(diff) + " on image" + str(i))
+print(value)
